a87fb476112c48af2cd9e752460e5fa111e8ec53
Alexey Papin <zzheads@gmail.com>
2017-01-22 09:10:37 +0300

    Delete Realm framework

---
+++ NASA/MarsRoversDataSource.swift
@@ -0,0 +1,15 @@
+//
+//  MarsRoversDataSource.swift
+//  NASA
+//
+//  Created by Alexey Papin on 22.01.17.
+//  Copyright © 2017 zzheads. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MarsRoversDataSource {
+    var rovers: [MarsRover] = []
+    var manifests: [MarsRoverPhotoManifest] = []
+}
